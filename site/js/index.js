a56e9fbc883ade6ce84fe91f893d472933ab0b4e
irinaterribilini <irina.terribilini@students.fhnw.ch>
2016-05-16 16:06:56 +0200

    fixed arrows for slider
    in css.
    
    just 2 of 4 photos are shown. Maybe something wrong with js-file (href,
    src)

--- site/js/index.js
+++ site/js/index.js
@@ -4,6 +4,7 @@
 $( document ).ready(function() {
     $(".button-collapse").sideNav();
     $('.slider').slider({full_width: true});
+
     $('.slider').slider({
         directionNav: false,
         controlNav: false
