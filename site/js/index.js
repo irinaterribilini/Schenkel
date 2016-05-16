d3051203757697101f2f82ac50f21d4a38fbbb23
irinaterribilini <irina.terribilini@students.fhnw.ch>
2016-05-16 13:54:27 +0200

    slider arrows, font

--- site/js/index.js
+++ site/js/index.js
@@ -4,5 +4,15 @@
 $( document ).ready(function() {
     $(".button-collapse").sideNav();
     $('.slider').slider({full_width: true});
+    $('.slider').slider({
+        directionNav: false,
+        controlNav: false
+    })
+
+    $('.prev, .next').on('click', function(){
+        var href = $(this).attr('href');
+        $('.slider').slider(href);
+        return false;
+    })
 
 })
